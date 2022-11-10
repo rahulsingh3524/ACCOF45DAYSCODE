5f42c696f7b4f63ee8307bee7b236a54d82c8efc
Rahul singh dhakad <50391360+rahulsingh3524@users.noreply.github.com>
2022-11-10 20:28:46 +0530

    Add files via upload

---
+++ Day 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,x,ans;
+	cin>>T;
+	for (int i=1;i<=T;i++){
+	  cin>>x;
+	  ans = x * 2 * 5;
+	  cout<<ans<<endl;
+	}
+	return 0;
+}
